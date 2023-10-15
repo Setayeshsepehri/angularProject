938878a6153c06280e5849c8fdb23116ca71d5d2
Setayeshsepehri <setayesh.spr@gmail.com>
2023-10-16 03:00:34 +0330

    angularProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'generatePassword';
+  posts=[
+    {
+      tile: 'tree',
+      imageUrl: 'assets/image/4k-windowsiran-com-2.jpg' ,
+      username: 'nature',
+      content: 'i saw neat tree'
+    },
+    {
+      tile: 'mountion',
+      imageUrl: 'assets/image/4k-windowsiran-com-5.jpg' ,
+      username: 'mountion',
+      content: 'i saw neat mountion'
+    },
+    {
+      tile: 'bikking',
+      imageUrl: 'assets/image/4k-windowsiran-com-6.jpg' ,
+      username: 'bikking',
+      content: 'i saw neat bikking'
+    },
+    {
+      tile: 'tree',
+      imageUrl: 'assets/image/4k-windowsiran-com-2.jpg' ,
+      username: 'nature',
+      content: 'i saw neat tree'
+    },
+
+
+  ];
+  getposts(){
+    return this.posts
+  }
+  
 }
+
