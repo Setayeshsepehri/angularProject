336af6a62d10cc241398b60cdf8361f346c8ce0b
Setayeshsepehri <setayesh.spr@gmail.com>
2023-10-16 12:12:21 +0330

    TypingProject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { TypingComponent } from './typing/typing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    TypingComponent
   ],
   imports: [
     BrowserModule
